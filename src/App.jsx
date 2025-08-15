a7b3aba7233a880753bec1775634d1c42e55d66b
shandiru <msddiru@gmail.com>
2025-08-15 13:39:19 +0530

    change file names

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import Wash from "./components/Expertise";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Service from "./components/Heritage";
-import Qa from "./components/Journey";
+import Service from "./components/Services";
+import Qa from "./components/WhyChoose";
 import Navbar from "./components/Navbar";
-import Call from "./components/Story";
+import Call from "./components/CallToAction";
 import TestimonialSection from "./components/TestimonialSection";
 import ContactSection from "./components/ContactSection"; // Import the contact component
 
